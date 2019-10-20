e9572b8c0ca10614165349d591830c01ccca979f
hotaru <36354847+hotaru51@users.noreply.github.com>
2019-10-20 21:27:54 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'ships/index'
-  get 'ships/show'
-  get 'ships/new'
-  get 'ships/edit'
+  root to: 'ships#index'
+  get 'ships', to: 'ships#index'
+  get 'ships/:id', to: 'ships#show'
+  get 'ships/new', to: 'ships#new'
+  get 'ships/:id/edit', to: 'ships#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
