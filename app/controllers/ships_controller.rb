30d5c2eefc8d174caab95a402f30caf5990001b6
hotaru <36354847+hotaru51@users.noreply.github.com>
2019-10-21 00:28:35 +0900

    ship登録、一覧表示実装

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,13 +1,34 @@
 class ShipsController < ApplicationController
   def index
+    @ships = Ship.all
   end
 
   def show
   end
 
   def new
+    @ship = Ship.new(ship_name: generate_ship)
   end
 
   def edit
+  end
+
+  def create
+    ship = Ship.new(ship_params)
+    ship.save!
+    redirect_to ships_path
+  end
+
+  private
+  def generate_ship
+    otonokizaka = %w(まき りん ぱな ほの こと うみ のぞ えり にこ)
+    uranohoshi  = %w(るび まる よし ちか よう りこ かな まり だい)
+    nijigaku    = %w(しず かす りな あゆ あい せつ かり かな えま)
+
+    (otonokizaka + uranohoshi + nijigaku).sample(2).join
+  end
+
+  def ship_params
+    params.require(:ship).permit(:ship_name, :comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'ships#index'
   get 'ships', to: 'ships#index'
-  get 'ships/:id', to: 'ships#show'
+  post 'ships', to: 'ships#create'
   get 'ships/new', to: 'ships#new'
+  get 'ships/:id', to: 'ships#show'
   get 'ships/:id/edit', to: 'ships#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
